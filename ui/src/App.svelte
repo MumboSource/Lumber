67e3c6029d3ccbd3b86d279b39adfe10ff581a8e
Ruadhan KellerMcLeer <ruadhan.kellermcleer@gmail.com>
2025-01-25 13:45:54 -0500

    add tailwind

---
+++ ui/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import svelteLogo from "./assets/svelte.svg";
+	import viteLogo from "/vite.svg";
+	import Counter from "./lib/Counter.svelte";
+	import electronLogo from "./assets/electron.svg";
+	import tailwindLogo from "./assets/tailwind.svg";
+
+	const nodeVersion = api.node();
+	const chromeVersion = api.chrome();
+	const electronVersion = api.electron();
+</script>
+
+<main class="w-full h-full">
+	Hi
+</main>
