858e0f916a8567f99ea967e0cb762c1c9ff341f3
TesDevelopment <joshuaepee1@gmail.com>
2025-01-26 02:14:40 -0500

    lerp!

--- ui/src/lib/pages/LoadingPage.svelte
+++ ui/src/lib/pages/LoadingPage.svelte
@@ -1,9 +1,14 @@
 <script>
+    import { ipcRenderer } from "electron";
     import { navigate } from "svelte-routing";
 
-    setTimeout(() => {
-        navigate("/exe-list") 
-    }, 1000)
+    ipcRenderer.on("heartbeat", (e, ...args) => {
+        console.log("It lives!")
+    })
+
+    // setTimeout(() => {
+    //     navigate("/exe-list") 
+    // }, 1000)
 </script>
 
 <div class="flex flex-col w-screen h-screen items-center justify-center gap-6">
