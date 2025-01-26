f6c46cf62e08b227b206ee78f7aa72fa22bccd77
Joshua Bryan Epee Thonga <20094920@students.k12.dc.us>
2025-01-25 21:48:17 -0500

    sidebar tweaks

--- ui/src/lib/Sidebar.svelte
+++ ui/src/lib/Sidebar.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <div class="w-72 border-[#E0E0E0] border-r-2 flex flex-col pt-2">
-    <SidebarItem to="#" iconName="appinfo" selected={selectedIndex == 0} text="Application Info"/>
-    <SidebarItem to="#" iconName="registry" selected={selectedIndex == 1} text="Registry Integrity"/>
-    <SidebarItem to="#" iconName="suspicious" selected={selectedIndex == 2} text="Suspicious Executables"/>
-    <SidebarItem to="#" iconName="activity" selected={selectedIndex == 3} text="Activity Analysis"/>
-    <SidebarItem to="#" iconName="file" selected={selectedIndex == 4} text="File Actions"/>
-    <SidebarItem to="#" iconName="system-information" selected={selectedIndex == 5} text="System Information"/>
-    <SidebarItem to="#" iconName="flowchart" selected={selectedIndex == 6} text="Flow Chart"/>
+    <SidebarItem to="/application-info" iconName="appinfo" selected={selectedIndex == 0} text="Application Info"/>
+    <SidebarItem to="/registry-integrity" iconName="registry" selected={selectedIndex == 1} text="Registry Integrity"/>
+    <SidebarItem to="/suspicious-executables" iconName="suspicious" selected={selectedIndex == 2} text="Suspicious Executables"/>
+    <SidebarItem to="/activity-analysis" iconName="activity" selected={selectedIndex == 3} text="Activity Analysis"/>
+    <SidebarItem to="/file-actions" iconName="file" selected={selectedIndex == 4} text="File Actions"/>
+    <SidebarItem to="/system-information" iconName="system-information" selected={selectedIndex == 5} text="System Information"/>
+    <SidebarItem to="/flow-chart" iconName="flowchart" selected={selectedIndex == 6} text="Flow Chart"/>
 </div>
--- ui/src/lib/SidebarItem.svelte
+++ ui/src/lib/SidebarItem.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <Link to={url}>
-    <div class="flex flex-row text-xl gap-2 px-3 py-4 cursor-default {selected?"border-blue-500 border-l-4 bg-[#F6F6F6] font-bold":""}">
+    <div class="flex flex-row text-xl gap-2 px-3 py-4 {selected?"border-blue-500 border-l-4 bg-[#F6F6F6] font-bold cursor-default":""}">
         <img src="/{iconName}{selected?"-active":""}.svg" alt="" class="w-8 h-8">
         <p>{text}</p>
     </div>
