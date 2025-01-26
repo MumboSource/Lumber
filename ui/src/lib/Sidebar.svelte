ec1e78126331a49b2a17ca32a6443233ab84bbae
Ruadhan KellerMcLeer <ruadhan.kellermcleer@gmail.com>
2025-01-26 06:00:51 -0500

    rename stuff

--- ui/src/lib/Sidebar.svelte
+++ ui/src/lib/Sidebar.svelte
@@ -4,10 +4,10 @@
 
 <div class="w-72 border-[#E0E0E0] border-r-2 flex flex-col pt-2">
     <SidebarItem url="/exe-info" iconName="appinfo" text="Application Info" />
-    <SidebarItem url="/sus-exe" iconName="suspicious" text="Suspicious Executables"/>
+    <SidebarItem url="/sus-exe" iconName="suspicious" text="Executables"/>
     <SidebarItem url="/activity-analysis" iconName="activity" text="Activity Analysis"/>
     <SidebarItem url="/file-actions" iconName="file" text="File Actions"/>
-    <SidebarItem url="/sys-info" iconName="system-information" text="System Information"/>
+    <SidebarItem url="/sys-info" iconName="system-information" text="System Info"/>
     <SidebarItem url="/flow-chart" iconName="flowchart" text="Flow Chart"/>
 
     <div class="flex-grow w-full border-[#E0E0E0] border-b-2"></div>
