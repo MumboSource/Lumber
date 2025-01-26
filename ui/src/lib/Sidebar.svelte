4703985c276753766270314b371c29a8f5e64cc5
Joshua Bryan Epee Thonga <20094920@students.k12.dc.us>
2025-01-26 07:23:58 -0500

    Remove activity analysis

--- ui/src/lib/Sidebar.svelte
+++ ui/src/lib/Sidebar.svelte
@@ -5,7 +5,6 @@
 <div class="w-72 border-[#E0E0E0] border-r-2 flex flex-col pt-2">
     <SidebarItem url="/exe-info" iconName="appinfo" text="Application Info" />
     <SidebarItem url="/sus-exe" iconName="suspicious" text="Executables"/>
-    <SidebarItem url="/activity-analysis" iconName="activity" text="Activity Analysis"/>
     <SidebarItem url="/file-events" iconName="file" text="File Events"/>
     <SidebarItem url="/sys-info" iconName="system-information" text="System Info"/>
     <SidebarItem url="/flow-chart" iconName="flowchart" text="Flow Chart"/>
