8eb43b6ba2721126e7dce4e896112762a3dda6bf
Joshua Bryan Epee Thonga <20094920@students.k12.dc.us>
2025-01-25 23:41:08 -0500

    finish application info layout

---
+++ ui/src/lib/InfoSquare.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let color;
+    export let iconName;
+    export let number;
+    export let text;
+</script>
+
+<div class="h-0 pb-[100%] rounded-md relative" style="background-color: {color}">
+    <div class="flex flex-col p-6 absolute w-full h-full">
+        <div class="w-full flex-grow flex justify-end">
+            <img src="{iconName}.svg" alt="" class="w-8 h-8">
+        </div>
+        <p class="font-bold text-5xl text-white">{number}</p>
+        <p class="opacity-65 text-lg text-white">{text}</p>
+    </div>
+</div>
--- ui/src/lib/pages/ApplicationInfo.svelte
+++ ui/src/lib/pages/ApplicationInfo.svelte
@@ -1,4 +1,5 @@
 <script>
+	import InfoSquare from './../InfoSquare.svelte';
     import Sidebar from "../Sidebar.svelte";
     import MainNavbar from "../main-navbar.svelte";
 	import '@fontsource/lato';
@@ -8,9 +9,29 @@
     <MainNavbar />
     <div class="flex flex-row w-full flex-grow">
         <Sidebar />
-        <div class="flex flex-row p-5 gap-6">
-            <div class="flex flex-col gap-2">
+        <div class="flex flex-row p-5 gap-6 flex-grow">
+            <div class="flex flex-col gap-6 flex-1">
                 <p class="font-mono bg-[#F0F0F0] py-3 px-4 rounded-md text-3xl w-full">McAfee_CRACK.exe</p>
+                {#each {length: 15} as i}
+                    <div>
+                        <p class="opacity-50">Date Created</p>
+                        <p class="text-xl">12/13/24 10:20:30 EST</p>
+                    </div>
+                {/each}
+            </div>
+            <div class="flex flex-col gap-6 flex-1">
+                <div class="grid grid-cols-2 grid-rows-2 gap-2">
+                    <InfoSquare color="#4394EB" number="12" text="Files Accessed" iconName="accessed"/>
+                    <InfoSquare color="#1875D9" number="2" text="Executions" iconName="execution"/>
+                    <InfoSquare color="#125CAC" number="17" text="Actions Incurred" iconName="actions"/>
+                    <InfoSquare color="#074281" number="4" text="Child Processes" iconName="processes"/>
+                </div>
+                {#each {length: 10} as i}
+                    <div>
+                        <p class="opacity-50">Date Created</p>
+                        <p class="text-xl">12/13/24 10:20:30 EST</p>
+                    </div>
+                {/each}
             </div>
         </div>
     </div>
