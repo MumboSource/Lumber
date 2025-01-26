11c04bb84887d01995d25420b14e6080a986ddbf
Ruadhan KellerMcLeer <ruadhan.kellermcleer@gmail.com>
2025-01-25 22:15:46 -0500

    holy flip josh has a gyatt

--- ui/src/lib/main-navbar.svelte
+++ ui/src/lib/main-navbar.svelte
@@ -2,11 +2,11 @@
     import testIcon from '../assets/test-img.png'
 </script>
 
-<div class="flex p-3 h-16 w-full gap-3 items-center" style="border-bottom: 2px solid #E1E1E1;">
-    <div class="min-w-8 max-w-8"><img class="w-full" src={testIcon} alt="icon"/></div>
-    <div class="text-2xl">The goat executable</div>
+<div class="flex h-16 w-full px-3 gap-3 items-center" style="border-bottom: 2px solid #E1E1E1;">
+    <div class="min-w-8 max-w-8 py-3"><img class="w-full" src={testIcon} alt="icon"/></div>
+    <div class="text-2xl grow">The goat executable</div>
 
-    <div class="justify-self-end" style="border-left: 2px solid #E1E1E1;">
+    <div class="justify-self-end flex w-28 h-full items-center justify-between" style="border-left: 2px solid #E1E1E1;">
         <div>_</div>
         <div>o</div>
         <div>x</div>
--- ui/src/lib/pages/ApplicationInfo.svelte
+++ ui/src/lib/pages/ApplicationInfo.svelte
@@ -1,8 +1,10 @@
 <script>
     import Sidebar from "../Sidebar.svelte";
+    import MainNavbar from "../main-navbar.svelte";
 	import '@fontsource/lato';
 </script>
 
+<MainNavbar />
 <div class="flex flex-row w-screen h-screen">
     <Sidebar />
     <div class="flex flex-row p-5"></div>
